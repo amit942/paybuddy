df90c0a1b3788ac21c83578e3cde9c8b62fc03ab
priyanka.dudani@snapdeal.com <priyanka.dudani@snapdeal.com>
2015-03-08 20:42:57 +0530

    missing file

--- src/main/java/com/ydp/helper/impl/SmsHelperServiceImpl.java
+++ src/main/java/com/ydp/helper/impl/SmsHelperServiceImpl.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 import com.ydp.helper.AbstractMessageHelperService;
 
 public class SmsHelperServiceImpl extends AbstractMessageHelperService {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(SmsHelperServiceImpl.class);
 
     @Override
@@ -26,7 +26,7 @@ public class SmsHelperServiceImpl extends AbstractMessageHelperService {
 
     public static void main(String[] args) {
         SmsHelperServiceImpl obj = new SmsHelperServiceImpl();
-        obj.sendMessage("9582392133", 'txn1', 'permalink1');
+        obj.sendMessage("9582392133", "txn1", "permalink1");
     }
 
 }
